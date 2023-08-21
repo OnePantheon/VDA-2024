6c604bb696676f13d9a74a88acbe8a03dcb9c40c
Logan TANN <logon313@hotmail.fr>
2023-08-22 00:32:03 +0200

    style(minor): rename vars

--- src/Sequences/06_RejoinsAsso.tsx
+++ src/Sequences/06_RejoinsAsso.tsx
@@ -1,15 +1,15 @@
 import {Img} from 'remotion';
 import {PoppinsFont} from '../shared/fonts';
-import {staticFile, Easing, AbsoluteFill, useCurrentFrame} from 'remotion';
-import {useInterpolate, useTime} from 'remotion-time';
+import {staticFile, Easing, AbsoluteFill} from 'remotion';
+import {useInterpolate} from 'remotion-time';
 
 function RejoinsAsso() {
-  const translateY = useInterpolate(['start', 'start + 0.4s'], [-20, 0], {
+  const translateEntrance = useInterpolate(['start', 'start + 0.4s'], [-20, 0], {
     extrapolateLeft: 'clamp',
     extrapolateRight: 'clamp',
     easing: Easing.out(Easing.ease),
   });
-  const width = useInterpolate(['start + 0.8s', 'start + 1.2s'], [0, 100], {
+  const underlineWidth = useInterpolate(['start + 0.8s', 'start + 1.2s'], [0, 100], {
     extrapolateLeft: 'clamp',
     extrapolateRight: 'clamp',
     easing: Easing.in(Easing.exp),
@@ -18,7 +18,7 @@ function RejoinsAsso() {
     <AbsoluteFill style={{fontFamily: PoppinsFont}} className="text-white">
       <Img src={staticFile('06_Campus.png')} />
       <div
-        style={{transform: `translateX(${translateY}%)`}}
+        style={{transform: `translateX(${translateEntrance}%)`}}
         className="absolute w-11/12 h-full top-0 right-0 flex flex-col justify-center items-start"
       >
         <p className="text-8xl font-medium ml-20 drop-shadow-md">Rejoins notre association</p>
@@ -26,13 +26,13 @@ function RejoinsAsso() {
           pour
           <span className="ml-[0.5ch] relative inline-block p-1">
             <span
-              style={{width: `${width}%`}}
+              style={{width: `${underlineWidth}%`}}
               className="absolute z-30 overflow-hidden top-0 left-0 h-full text-black p-1 drop-shadow"
             >
               1€ symbolique
             </span>
             <span className="relative z-20">1€ symbolique</span>
-            <span style={{width: `${width}%`}} className="absolute z-10 bg-yellow-300 top-0 left-0 h-full" />
+            <span style={{width: `${underlineWidth}%`}} className="absolute z-10 bg-yellow-300 top-0 left-0 h-full" />
           </span>
         </p>
       </div>
